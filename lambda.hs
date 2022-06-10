310adbf16d4569003e4c941cfa120baf017c06b1
noco <k-nitrate.1002@ezweb.ne.jp>
2022-06-10 23:01:08 +0900

    Update lambda.hs

--- lambda.hs
+++ lambda.hs
@@ -7,19 +7,17 @@ app :: Expr -> Expr -> Expr
 app (L x m) n = subst n x m
     where
         subst :: Expr -> [Char] -> Expr -> Expr
-        subst n x (V y)
-            | x == y = n
-            | otherwise = V y
+        subst n x (V y) = if x == y then n else V y
         subst n x (L y m)
             | x == y = L x m
-            | free y n = (\ z -> L z $ subst n x $ subst (V z) y m) $ head $ filter (\v -> not $ free v n) $ (y ++) . show <$> [0 ..]
+            | free n y = (\ z -> L z $ subst n x $ subst (V z) y m) $ head $ filter (not . free n) $ (y ++) . show <$> [0 ..]
             | otherwise = L y $ subst n x m
         subst n x (A m1 m2) = app (subst n x m1) $ subst n x m2
 
-        free :: [Char] -> Expr -> Bool
-        free x (V y) = x == y
-        free x (L y m) = x /= y && free x m
-        free x (A m1 m2) = free x m1 || free x m2
+        free :: Expr -> [Char] -> Bool
+        free (V y) x = x == y
+        free (L y m) x = x /= y && free m x
+        free (A m1 m2) x = free m1 x || free m2 x
 app m1 m2 = A m1 m2
 
 parse :: [Char] -> [Char]
@@ -34,11 +32,9 @@ parse = str . foldl1 app . parse' . tail . scanl (\ (_, i) x -> (x, i + fromEnum
         str (A m n) = str m ++ " (" ++ str n ++ ")"
 
         parse' :: [(Char, Int)] -> [Expr]
-        parse' (('(', i) : t) = (\ (x, y) -> foldl1 app (parse' x) : parse' (tail y)) $ span ((>= i) . snd) t
+        parse' (('(', i) : t) = (\ (x, y) -> foldl1 app (parse' x) : parse' y) $ span ((>= i) . snd) t
         parse' (('\\', _) : t) = (\ (x, y) -> [L (fst <$> x) $ foldl1 app $ parse' (dropWhile ((/= '.') . fst) y)]) $ span (isAlphaNum . fst) $ dropWhile (not . isAlpha . fst) t
-        parse' ((x, _) : t)
-            | isAlpha x = (\ (y, z) -> V (x : map fst y) : parse' z) $ span (isAlphaNum . fst) t
-            | otherwise = parse' t
+        parse' ((x, _) : t) = if isAlpha x then (\ (y, z) -> V (x : map fst y) : parse' z) $ span (isAlphaNum . fst) t else parse' t
         parse' _ = []
 
 main :: IO ()
