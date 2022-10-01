428b53f10a4991b64992808c9967c9d6670d8c85
noco <k-nitrate.1002@ezweb.ne.jp>
2022-10-01 11:38:57 +0900

    Update lambda.hs

--- lambda.hs
+++ lambda.hs
@@ -7,103 +7,89 @@ import Text.Parsec.Language
 import Text.Parsec.String
 import Text.Parsec.Token
 
-data Expr = C Expr Expr | Id | P Expr Expr | P1 | P2 | L Expr | Ev | V [Char]
+data Expr = P1 | P2 | P [Expr] [Expr] | Ev | L [Expr] | V [Char]
 
-beta :: Expr -> Expr
-beta (C x y) = case (beta x, beta y) of
-        (C x y, z) -> beta . C x . beta $ C y z
-        (Id, x) -> x
-        (x, Id) -> x
-        (P1, P x _) -> x
-        (P2, P _ x) -> x
-        (P x y, z) -> P (beta $ C x z) . beta $ C y z
-        (Ev, P (L x) y) -> beta . C x $ P Id y
-        (L x, y) -> L . beta . C x $ P (beta $ C y P1) P2
-        (x, y) -> C x y
-beta (P x y) = P (beta x) $ beta y
-beta (L x) = L $ beta x
-beta x = x
+c :: Expr -> [Expr] -> [Expr]
+c P1 [P x _] = x
+c P2 [P _ x] = x
+c (P x y) z = [P (foldr c z x) $ foldr c z y]
+c Ev [P [L x] y] = foldr c [P [] y] x
+c (L x) y = [L $ foldr c [P (foldr c [P1] y) [P2]] x]
+c x y = x : y
 
-eta :: Expr -> Expr
-eta (C x y) = case eta y of
-        C y z -> C (eta $ C x y) z
-        y -> C (eta x) y
-eta (P x y) = case (eta x, eta y) of
-        (C x P1, C y P1) -> C (eta $ P x y) P1
-        (x, y) -> P x y
-eta (L x) = case eta x of
-        C Ev (P (C x P1) P2) -> x
-        x -> L x
-eta x = x
+eta :: [Expr] -> [Expr]
+eta = reverseE . (reverseE >=> eta')
+    where
+        eta' :: Expr -> [Expr]
+        eta' (P x y) = case (x >>= eta', y >>= eta') of
+                (P1 : x, P1 : y) -> P1 : eta' (P x y)
+                (x, y) -> [P x y]
+        eta' (L x) = case x >>= eta' of
+                [P (P1 : x) [P2], Ev] -> x
+                x -> [L x]
+        eta' x = [x]
 
-parseE :: [Char] -> Either ParseError Expr
+        reverseE :: [Expr] -> [Expr]
+        reverseE = reverse . map reverseE'
+        
+        reverseE' :: Expr -> Expr
+        reverseE' (P x y) = P (reverseE x) $ reverseE y
+        reverseE' (L x) = L $ reverseE x
+        reverseE' x = x
+
+parseE :: [Char] -> Either ParseError [Expr]
 parseE = parse (do
         whiteSpace lexer
-        x <- expr
+        x <- expr []
         eof
-        return $ free 0 x
+        return x
     ) ""
     where
         lexer :: TokenParser ()
         lexer = makeTokenParser haskellStyle
 
-        expr :: Parser Expr
-        expr = do
-                xs <- many1 $ parens lexer expr <|> fun <|> var
-                return $ foldl1 ((C Ev .) . P) xs
+        expr :: [[Char]] -> Parser [Expr]
+        expr ss = do
+                xs <- many1 $ parens lexer (expr ss) <|> fun ss <|> var ss
+                return $ foldl1 (\ x y -> c Ev [P x y]) xs
 
-        fun :: Parser Expr
-        fun = do
+        fun :: [[Char]] -> Parser [Expr]
+        fun ss = do
                 symbol lexer "\\"
-                ss <- many $ identifier lexer <|> symbol lexer "_"
+                ts <- many $ identifier lexer <|> symbol lexer "_"
                 dot lexer
-                x <- expr
-                return $ foldr ((L .) . bind 0) x ss
+                x <- expr $ reverse ts ++ ss
+                return $ foldr (const $ (: []) . L) x ts
 
-        var :: Parser Expr
-        var = do
+        var :: [[Char]] -> Parser [Expr]
+        var ss = do
                 s <- identifier lexer
-                return $ V s
+                return $ maybe (V s : map (const P1) ss) ((P2 :) . (`replicate` P1)) $ elemIndex s ss
 
-        bind :: Int -> [Char] -> Expr -> Expr
-        bind i s (C Ev (P x y)) = C Ev . P (bind i s x) $ bind i s y
-        bind i s (L x) = L $ bind (i + 1) s x
-        bind i s (V t) | s == t = C P2 $ iterate (C P1) Id !! i
-        bind _ _ x = x
-
-        free :: Int -> Expr -> Expr
-        free i (C Ev (P x y)) = C Ev . P (free i x) $ free i y
-        free i (L x) = L $ free (i + 1) x
-        free i (V t) = C (V t) $ iterate (C P1) Id !! i
-        free _ x = x
-
-showE :: Expr -> [Char]
+showE :: [Expr] -> [Char]
 showE x = showE' 0 x
     where
-        showE' :: Int -> Expr -> [Char]
-        showE' i (C Ev (P x (C Ev y))) = showE' i x ++ '(' : showE' i (C Ev y) ++ ")"
-        showE' i (C Ev (P x (L y))) = showE' i x ++ '(' : showE' i (L y) ++ ")"
-        showE' i (C Ev (P x y)) = showE' i x ++ ' ' : showE' i y
-        showE' i (C (V s) _) = s
-        showE' i (C x P1) = showE' (i - 1) x
-        showE' i (C _ x) = showE' (i - 1) x
-        showE' i (L x) = '\\' : showL i x
-        showE' i (V s) = s
-        showE' i _ = name !! (i - 1)
+        showE' :: Int -> [Expr] -> [Char]
+        showE' i [Ev, P x (Ev : y)] = showE' i x ++ '(' : showE' i (Ev : y) ++ ")"
+        showE' i [Ev, P x [L y]] = showE' i x ++ '(' : showE' i [L y] ++ ")"
+        showE' i [Ev, P x y] = showE' i x ++ ' ' : showE' i y
+        showE' i [L x] = '\\' : showL i x
+        showE' i (V s : _) = s
+        showE' i (P2 : x) = name !! (i - 1 - length x)
+        showE' i x = showE' (i - 1) $ init x
 
-        showL :: Int -> Expr -> [Char]
-        showL i (L x) = name !! i ++ ' ' : showL (i + 1) x
+        showL :: Int -> [Expr] -> [Char]
+        showL i [L x] = name !! i ++ ' ' : showL (i + 1) x
         showL i x = name !! i ++ '.' : showE' (i + 1) x
 
         name :: [[Char]]
         name = "" : map show [1 ..] >>= filter (`notElem` free x) . (<$> ['a' .. 'z']) . flip (:)
 
-        free :: Expr -> [[Char]]
-        free (C Ev x) = free x
-        free (C x _) = free x
-        free (P x y) = free x `union` free y
-        free (L x) = free x
-        free (V s) = [s]
+        free :: [Expr] -> [[Char]]
+        free (Ev : x) = free x
+        free (P x y : _) = free x `union` free y
+        free (L x : _) = free x
+        free (V s : _) = [s]
         free _ = []
 
 main :: IO ()
@@ -115,4 +101,4 @@ main = do
         case args' of
                 [] -> forever . (putStr "> " >> hFlush stdout >> getLine >>=)
                 ss -> forM_ ss . (readFile >=>)
-            $ either print (putStrLn . showE . (if b then id else eta) . beta) . parseE
+            $ either print (putStrLn . showE . if b then id else eta) . parseE
