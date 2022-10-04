fd5e70adb50057dd3f0bf1c44e1d70dafc9f2522
noco <k-nitrate.1002@ezweb.ne.jp>
2022-10-04 23:06:55 +0900

    Update lambda.hs

--- lambda.hs
+++ lambda.hs
@@ -7,40 +7,40 @@ import Text.Parsec.Language
 import Text.Parsec.String
 import Text.Parsec.Token
 
-data Term = U | F | S | P [Term] [Term] | A | L [Term] | V [Char]
+data Term = Ignore | Fst | Snd | Pair [Term] [Term] | App | Curry [Term] | Var [Char]
 
-c :: Term -> [Term] -> [Term]
-c U _ = [U]
-c F [P x _] = x
-c S [P _ x] = x
-c (P x y) z = [P (foldr c z x) $ foldr c z y]
-c A [P [L x] y] = foldr c [P [] y] x
-c (L x) y = [L $ foldr c [P (foldr c [F] y) [S]] x]
-c x y = x : y
+comp :: Term -> [Term] -> [Term]
+comp Ignore _ = [Ignore]
+comp Fst [Pair x _] = x
+comp Snd [Pair _ x] = x
+comp (Pair x y) z = [Pair (foldr comp z x) $ foldr comp z y]
+comp App [Pair [Curry x] y] = foldr comp [Pair [] y] x
+comp (Curry x) y = [Curry $ foldr comp [Pair (foldr comp [Fst] y) [Snd]] x]
+comp x y = x : y
 
 eta :: [Term] -> [Term]
-eta = reverseE . (reverseE >=> eta')
+eta = opposite . (opposite >=> eta')
     where
         eta' :: Term -> [Term]
-        eta' (P x y) = case (x >>= eta', y >>= eta') of
-                (U : x, U : y) -> [U, P x y]
-                (U : x, F : y) -> F : eta' (P (U : x) y)
-                (F : x, U : y) -> F : eta' (P x $ U : y)
-                (F : x, F : y) -> F : eta' (P x y)
-                (x, y) -> [P x y]
-        eta' (L x) = case x >>= eta' of
-                [P (U : x) [S], A] -> U : x
-                [P (F : x) [S], A] -> x
-                x -> [L x]
+        eta' (Pair x y) = case (x >>= eta', y >>= eta') of
+                (Ignore : x, Ignore : y) -> [Ignore, Pair x y]
+                (Ignore : x, Fst : y) -> Fst : eta' (Pair (Ignore : x) y)
+                (Fst : x, Ignore : y) -> Fst : eta' (Pair x $ Ignore : y)
+                (Fst : x, Fst : y) -> Fst : eta' (Pair x y)
+                (x, y) -> [Pair x y]
+        eta' (Curry x) = case x >>= eta' of
+                [Pair (Ignore : x) [Snd], App] -> Ignore : x
+                [Pair (Fst : x) [Snd], App] -> x
+                x -> [Curry x]
         eta' x = [x]
 
-        reverseE :: [Term] -> [Term]
-        reverseE = reverse . map reverseT
+        opposite :: [Term] -> [Term]
+        opposite = reverse . map inverse
         
-        reverseT :: Term -> Term
-        reverseT (P x y) = P (reverseE x) $ reverseE y
-        reverseT (L x) = L $ reverseE x
-        reverseT x = x
+        inverse :: Term -> Term
+        inverse (Pair x y) = Pair (opposite x) $ opposite y
+        inverse (Curry x) = Curry $ opposite x
+        inverse x = x
 
 parseE :: [Char] -> Either ParseError [Term]
 parseE = parse (do
@@ -56,7 +56,7 @@ parseE = parse (do
         expr :: [[Char]] -> Parser [Term]
         expr ss = do
                 xs <- many1 $ parens lexer (expr ss) <|> fun ss <|> var ss
-                return $ foldl1 (\ x y -> c A [P x y]) xs
+                return $ foldl1 (\ x y -> comp App [Pair x y]) xs
 
         fun :: [[Char]] -> Parser [Term]
         fun ss = do
@@ -64,37 +64,37 @@ parseE = parse (do
                 ts <- many1 $ identifier lexer <|> symbol lexer "_"
                 dot lexer
                 x <- expr $ reverse ts ++ ss
-                return $ foldr (const $ (: []) . L) x ts
+                return $ foldr (const $ (: []) . Curry) x ts
 
         var :: [[Char]] -> Parser [Term]
         var ss = do
                 s <- identifier lexer
-                return $ maybe [V s, U] ((S :) . (`replicate` F)) $ elemIndex s ss
+                return $ maybe [Var s, Ignore] ((Snd :) . (`replicate` Fst)) $ elemIndex s ss
 
 showE :: [Term] -> [Char]
 showE x = showE' 0 x
     where
         showE' :: Int -> [Term] -> [Char]
-        showE' i [A, P x (A : y)] = showE' i x ++ '(' : showE' i (A : y) ++ ")"
-        showE' i [A, P x [L y]] = showE' i x ++ '(' : showE' i [L y] ++ ")"
-        showE' i [A, P x y] = showE' i x ++ ' ' : showE' i y
-        showE' i [L x] = '\\' : showL i x
-        showE' i (V s : _) = s
-        showE' i (S : x) = name !! (i - 1 - length x)
+        showE' i [App, Pair x (App : y)] = showE' i x ++ '(' : showE' i (App : y) ++ ")"
+        showE' i [App, Pair x [Curry y]] = showE' i x ++ '(' : showE' i [Curry y] ++ ")"
+        showE' i [App, Pair x y] = showE' i x ++ ' ' : showE' i y
+        showE' i [Curry x] = '\\' : showL i x
+        showE' i (Var s : _) = s
+        showE' i (Snd : x) = name !! (i - 1 - length x)
         showE' i x = showE' (i - 1) $ init x
 
         showL :: Int -> [Term] -> [Char]
-        showL i [L x] = name !! i ++ ' ' : showL (i + 1) x
+        showL i [Curry x] = name !! i ++ ' ' : showL (i + 1) x
         showL i x = name !! i ++ '.' : showE' (i + 1) x
 
         name :: [[Char]]
         name = "" : map show [1 ..] >>= filter (`notElem` free x) . (<$> ['a' .. 'z']) . flip (:)
 
         free :: [Term] -> [[Char]]
-        free (A : x) = free x
-        free (P x y : _) = free x `union` free y
-        free (L x : _) = free x
-        free (V s : _) = [s]
+        free (App : x) = free x
+        free (Pair x y : _) = free x `union` free y
+        free (Curry x : _) = free x
+        free (Var s : _) = [s]
         free _ = []
 
 main :: IO ()
